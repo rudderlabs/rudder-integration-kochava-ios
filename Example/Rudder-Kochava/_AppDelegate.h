4de52f886963f5cf4a3a83469931608192bd61d3
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-05-29 16:23:29 +0530

    chore: user notification import

--- Example/Rudder-Kochava/_AppDelegate.h
+++ Example/Rudder-Kochava/_AppDelegate.h
@@ -6,11 +6,10 @@
 //  Copyright (c) 2021 Desu Sai Venkat. All rights reserved.
 //
 
-#import <UserNotifications/UserNotifications.h>
 
 @import UIKit;
 
-@interface _AppDelegate : UIResponder <UIApplicationDelegate, UNUserNotificationCenterDelegate>
+@interface _AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
